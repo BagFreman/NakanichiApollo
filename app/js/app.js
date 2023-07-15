52d3bf9d75e6761024ce2577114bf7f1f01958c0
BagFreman <mr.bagfreman@gmail.com>
2023-07-16 01:03:24 +0300

    update

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,59 @@
 document.addEventListener('DOMContentLoaded', () => {
 
 
+   gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
+   ScrollSmoother.create({
+      wrapper: '.wrapper',
+      content: '.content'
+   })
+
+   window.addEventListener('scroll', e => {
+      document.documentElement.style.setProperty('--scrollTop', `${this.scrollY}px`) // Update method
+   })
+
+
+   if (window.innerWidth > 992) {
+
+      gsap.to(".page-main-offer-text__img-2", {
+         y: 100,
+         transition: "linear",
+         scrollTrigger: {
+            trigger: ".page-main-offer-text",
+            start: "top 50%",
+            end: "bottom 100%",
+            scrub: 2,
+         }
+      })
+
+      gsap.to(".page-main-offer-text__img-1", {
+         y: 50,
+         transition: "linear",
+         scrollTrigger: {
+            trigger: ".page-main-offer-text",
+            start: "top 50%",
+            end: "bottom 100%",
+            scrub: 2,
+         }
+      })
+
+      window.addEventListener('load', function () {
+         // выбираем все элементы, которым нужно добавить класс
+         const elements = document.querySelectorAll('.page-main-menu-items a');
+   
+         // добавляем классы к элементам
+         this.setTimeout(function () {
+            elements.forEach(function (element, i) {
+               element.classList.add('animate-' + ++i);
+            });
+         }, 1000);
+   
+      });
+
+
+   }
+
+
+
+
 
 })
