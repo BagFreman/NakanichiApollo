97e7cc0c177d5a5b7a7811c6d867e1d8276bac6f
BagFreman <mr.bagfreman@gmail.com>
2023-07-16 18:51:29 +0300

    update

--- app/js/app.js
+++ app/js/app.js
@@ -15,7 +15,7 @@ document.addEventListener('DOMContentLoaded', () => {
    if (window.innerWidth > 992) {
 
       gsap.to(".page-main-offer-text__img-2", {
-         y: 100,
+         y: 250,
          transition: "linear",
          scrollTrigger: {
             trigger: ".page-main-offer-text",
@@ -26,7 +26,7 @@ document.addEventListener('DOMContentLoaded', () => {
       })
 
       gsap.to(".page-main-offer-text__img-1", {
-         y: 50,
+         y: 150,
          transition: "linear",
          scrollTrigger: {
             trigger: ".page-main-offer-text",
@@ -37,23 +37,63 @@ document.addEventListener('DOMContentLoaded', () => {
       })
 
       window.addEventListener('load', function () {
-         // выбираем все элементы, которым нужно добавить класс
          const elements = document.querySelectorAll('.page-main-menu-items a');
-   
-         // добавляем классы к элементам
          this.setTimeout(function () {
             elements.forEach(function (element, i) {
                element.classList.add('animate-' + ++i);
             });
-         }, 1000);
-   
+         }, 500);
       });
 
+      // Создаем элемент для кастомной мыши
+      var customCursor = document.createElement('div');
+      customCursor.style.position = 'fixed';
+      customCursor.style.transform = 'translate(-50%, -50%)'
+      customCursor.style.width = '220px';
+      customCursor.style.height = '220px';
+      customCursor.style.opacity = '0';
+      customCursor.style.transition = 'opacity .3s ease-in-out';
+      customCursor.style.backgroundSize = 'cover';
+      customCursor.style.pointerEvents = 'none';
+      document.body.appendChild(customCursor);
+
+      // Отслеживаем положение курсора мыши во время скролла
+      document.body.addEventListener('mousemove', function (event) {
+         customCursor.style.left = event.clientX + 'px';
+         customCursor.style.top = event.clientY + 'px';
+      });
 
    }
 
+   var galleryImg1 = document.querySelector('.gallery-img__img-1');
 
+   galleryImg1.addEventListener('mouseover', function () {
+      customCursor.classList.add('gallery-img-1');
+   });
 
+   galleryImg1.addEventListener('mouseout', function () {
+      customCursor.classList.remove('gallery-img-1');
+   });
 
+   var galleryImg2 = document.querySelector('.gallery-img__img-2');
+
+   galleryImg2.addEventListener('mouseover', function () {
+      customCursor.classList.add('gallery-img-2');
+   });
+
+   galleryImg2.addEventListener('mouseout', function () {
+      customCursor.classList.remove('gallery-img-2');
+   });
+
+   var galleryImg3 = document.querySelector('.gallery-img__img-3');
+
+   galleryImg3.addEventListener('mouseover', function () {
+      customCursor.classList.add('gallery-img-3');
+   });
+
+   galleryImg3.addEventListener('mouseout', function () {
+      customCursor.classList.remove('gallery-img-3');
+   });
 
 })
+
